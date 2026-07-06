7995b52044815bc3e90682cb783be91fea4f12da
bazmatic <barry@earsman.com>
2026-07-06 16:42:43 +1000

    feat: add LocalLangChainEmbeddings adapter for MemoryVectorStore

---
+++ src/rag/LocalLangChainEmbeddings.ts
@@ -0,0 +1,16 @@
+import { Embeddings } from "langchain/embeddings/base";
+import { embedText, embedTexts } from "./local-embeddings";
+
+export class LocalLangChainEmbeddings extends Embeddings {
+  constructor() {
+    super({});
+  }
+
+  async embedDocuments(documents: string[]): Promise<number[][]> {
+    return embedTexts(documents);
+  }
+
+  async embedQuery(text: string): Promise<number[]> {
+    return embedText(text);
+  }
+}
